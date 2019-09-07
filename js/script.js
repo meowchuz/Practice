874e6bc688c2afb41ed628bc552dad4fb627c1fd
meowchuz <38089783+meowchuz@users.noreply.github.com>
2019-09-06 20:07:51 -0700

    Add comment

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
     console.log(data);
-
+//Practice
     var customer_list = data;
     customer_list.forEach(function(customer)
     {
         $("#customers-list").append(
         "<li>" + customer.name + 
             "<ul>" + 
-                "<li>" + customer.birthdate + "</li>" + 
+                "<li>" + customer.birthdate + "</li>" +
+
             "</ul>" +
         "</li>");
     });
