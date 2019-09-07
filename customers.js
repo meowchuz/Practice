fd6bf9b4cf151ad8b066ebe786ac59c31337505a
meowchuz <38089783+meowchuz@users.noreply.github.com>
2019-09-06 19:57:38 -0700

    First Practice - Append JQuery into HTML

---
+++ customers.js
@@ -0,0 +1,68 @@
+var data = [
+    {
+        "id": 2,
+        "name": "Janey Diep",
+        "birthdate": "1992-09-22T00:00:00",
+        "isSubcribedToNewsletter": true,
+        "membershipTypeId": 2,
+        "membershipType": {
+            "id": 2,
+            "nameType": "Monthly"
+        }
+    },
+    {
+        "id": 3,
+        "name": "Joey Le",
+        "birthdate": "1995-08-10T00:00:00",
+        "isSubcribedToNewsletter": false,
+        "membershipTypeId": 1,
+        "membershipType": {
+            "id": 1,
+            "nameType": "Pay As You Go"
+        }
+    },
+    {
+        "id": 4,
+        "name": "Trung Le",
+        "birthdate": "1960-10-05T00:00:00",
+        "isSubcribedToNewsletter": true,
+        "membershipTypeId": 2,
+        "membershipType": {
+            "id": 2,
+            "nameType": "Monthly"
+        }
+    },
+    {
+        "id": 6,
+        "name": "Thoa Nguyen",
+        "birthdate": null,
+        "isSubcribedToNewsletter": true,
+        "membershipTypeId": 1,
+        "membershipType": {
+            "id": 1,
+            "nameType": "Pay As You Go"
+        }
+    },
+    {
+        "id": 7,
+        "name": "Heo Con",
+        "birthdate": "2004-06-30T00:00:00",
+        "isSubcribedToNewsletter": true,
+        "membershipTypeId": 1,
+        "membershipType": {
+            "id": 1,
+            "nameType": "Pay As You Go"
+        }
+    },
+    {
+        "id": 8,
+        "name": "Hien Nguyen",
+        "birthdate": "1994-04-25T00:00:00",
+        "isSubcribedToNewsletter": true,
+        "membershipTypeId": 2,
+        "membershipType": {
+            "id": 2,
+            "nameType": "Monthly"
+        }
+    }
+]
---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    console.log(data);
+
+    var customer_list = data;
+    customer_list.forEach(function(customer)
+    {
+        $("#customers-list").append(
+        "<li>" + customer.name + 
+            "<ul>" + 
+                "<li>" + customer.birthdate + "</li>" + 
+            "</ul>" +
+        "</li>");
+    });
+});
+    
