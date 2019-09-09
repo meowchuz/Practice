016d3b49d24cddbe0ff878cfd88e382baa888bfa
meowchuz <38089783+meowchuz@users.noreply.github.com>
2019-09-09 16:24:36 -0700

    Add Remove button

--- js/script.js
+++ js/script.js
@@ -2,15 +2,43 @@ $(document).ready(function(){
     console.log(data);
 //Practice
     var customer_list = data;
-    customer_list.forEach(function(customer)
-    {
-        $("#customers-list").append(
-        "<li>" + customer.name + 
-            "<ul>" + 
-                "<li>" + customer.birthdate + "</li>" +
 
-            "</ul>" +
-        "</li>");
+    if (!(customer_list == null)) {
+        var customer_table = "";
+        customer_table += "<table id='customerList' class='table table-bordered'>";
+        customer_table += "<thead class='thead-dark'>";
+        customer_table += 
+        "<th scope='col'> ID </th>" + 
+        "<th scope='col'> Name </th>" + 
+        "<th scope='col'> Birthday </th>" + 
+        "<th scope='col'> Is Subcribed </th>" + 
+        "<th scope='col'> Membership Type </th>" +
+        "<th scope='col'></th>";
+        customer_table += "</thead><tbody>";
+        customer_list.forEach(function(customer)
+        {
+            customer_table += 
+            "<tr>" +
+            "<td>" + customer.id + "</td>" +  
+            "<td>" + customer.name + "</td>" +
+            "<td>" + (customer.birthdate? customer.birthdate : "No data") + "</td>" +
+            "<td>" + (customer.isSubcribedToNewsletter? "Yes" : "No") + "</td>" +
+            "<td>" + customer.membershipType.nameType + "</td>" +
+            "<td><button type='button' id='btnRemove' class='btn btn-danger'>Remove</button></td>" +
+            "</tr>";
+        });
+        customer_table += "</tbody>";
+
+        $("#container").html(customer_table);
+    }
+    else 
+    {
+        $("#container").html("There is no record!!");
+    };
+
+    $("#customerList").on("click", "#btnRemove", function(){
+        $(this).parents("tr").remove();
     });
+
 });
     
